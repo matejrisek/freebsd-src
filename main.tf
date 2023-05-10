fcda5eea440fcbd88715dbd76de42b6502b87ce1
matej.risek <matej.risek@hashicorp.com>
2023-05-10 14:30:48 +0200

    changing

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "random_pet" "name" {
  separator = "-"
 }
 
-resource "null_resource" "sleeper3456" {
+resource "null_resource" "sleeper3457" {
   provisioner "local-exec" {
     command = "sleep 4"
   }
