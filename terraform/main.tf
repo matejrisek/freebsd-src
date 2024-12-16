68257ff838fc58d749ecaaf435769527bfc8174f
Matej Risek <matej.risek@hashicorp.com>
2024-12-16 09:23:48 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ resource "random_pet" "name" {
  separator = "-"
 }
 
-resource "null_resource" "sleeper" {
+resource "null_resource" "sleeper2" {
   provisioner "local-exec" {
     command = "sleep 4"
   }
