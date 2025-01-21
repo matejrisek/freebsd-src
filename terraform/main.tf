c7105cea6c2db790ab51fc5975b75f70d58ebec2
Matej Risek <matej.risek@hashicorp.com>
2025-01-21 11:13:53 +0100

    No Partial No FF

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ resource "random_pet" "name" {
  separator = "-"
 }
 
-resource "null_resource" "sleeper_no_partial" {
+resource "null_resource" "sleeper_no_partial_no_ff" {
   provisioner "local-exec" {
     command = "sleep 4"
   }
